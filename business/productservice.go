c8cef9b402b8fbdf53b2e64978afd3e2b98422a4
Hernan Tenjo <hernan.tenjo@mercadolibre.com.co>
2021-05-04 13:22:49 -0500

    Complex validations working with conditional schemas

---
+++ business/productservice.go
@@ -0,0 +1,8 @@
+package business
+
+import "fmt"
+
+func CreateProduct() error {
+	fmt.Printf("::: Some business logic")
+	return nil
+}
--- main.go
+++ main.go
@@ -7,11 +7,37 @@ import (
 )
 
 func main() {
-
 	schemaLoader := gojsonschema.NewReferenceLoader(canonicalPath("/json-test/paymentIntent-schema.json"))
-	documentLoader := gojsonschema.NewReferenceLoader(canonicalPath("/json-test/paymentIntent-example4.json"))
+	schema, _ := gojsonschema.NewSchema(schemaLoader)
 
-	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	validateJsonFromFile(schema, "/json-test/001-paymentIntent-required-ok.json")
+	validateJsonFromFile(schema, "/json-test/002-paymentIntent-required-error.json")
+	validateJsonFromFile(schema, "/json-test/003-paymentIntent-enum-ok.json")
+	validateJsonFromFile(schema, "/json-test/004-paymentIntent-enum-error.json")
+	validateJsonFromFile(schema, "/json-test/005-paymentIntent-dependent-ok.json")
+	validateJsonFromFile(schema, "/json-test/006-paymentIntent-dependent-error.json")
+	validateJsonFromFile(schema, "/json-test/007-paymentIntent-dependent-by-values-credit-ok.json")
+	validateJsonFromFile(schema, "/json-test/008-paymentIntent-dependent-by-values-debit-ok.json")
+	validateJsonFromFile(schema, "/json-test/009-paymentIntent-dependent-by-values-error.json")
+	validateJsonFromFile(schema, "/json-test/010-paymentIntent-dependent-by-values-min-max-error.json")
+}
+
+func canonicalPath(filePath string) string {
+	fileSchemaPrefix := "file://"
+	dirPath, _ := os.Getwd()
+	canonicalPath := fileSchemaPrefix + dirPath + filePath
+	//fmt.Printf("::: Canonical path: [%s]\n", canonicalPath)
+	return canonicalPath
+}
+
+func validateJsonFromFile(schema *gojsonschema.Schema, jsonFilePath string) {
+	fmt.Printf("\n::: Testing [%s]\n", jsonFilePath)
+	documentLoader := gojsonschema.NewReferenceLoader(canonicalPath(jsonFilePath))
+	validateJson(schema, documentLoader)
+}
+
+func validateJson(schema *gojsonschema.Schema, source gojsonschema.JSONLoader) {
+	result, err := schema.Validate(source)
 
 	if err != nil {
 		fmt.Printf("::: There was an error validating the schema: \n[%v]\n", err)
@@ -26,12 +52,4 @@ func main() {
 			fmt.Printf("- %s\n", desc)
 		}
 	}
-}
-
-func canonicalPath(filePath string) string {
-	fileSchemaPrefix := "file://"
-	dirPath, _ := os.Getwd()
-	canonicalPath := fileSchemaPrefix + dirPath + filePath
-	fmt.Printf("::: Canonical path: [%s]\n", canonicalPath)
-	return canonicalPath
 }
